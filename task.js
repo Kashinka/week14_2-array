00a9686cfefcd241c84f37236c6455edff32e529
Kashinka <azalena@yandex.ru>
2023-07-07 22:42:06 +0300

    Added task1

---
+++ task.js
@@ -0,0 +1,15 @@
+//task 1
+const guestList = ['Маша', 'Леонард', 'Шелдон', 'Джон Сноу'];
+
+// 1. Выведите в кносоль первого гостя
+const firstGuest = guestList[0];
+console.log(firstGuest);
+// Маша
+
+// 2. Выведите в кносоль третьего гостя
+const thirdGuest = guestList[2];
+console.log(thirdGuest);
+
+// 3. Выведите в консоль девятьсот девятого гостя
+const ninehundredninthGuest = guestList[908];
+console.log(ninehundredninthGuest);
