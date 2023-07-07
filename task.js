db2d644ac1504ca6e0c4a934a623852bb61c32c3
Kashinka <azalena@yandex.ru>
2023-07-07 23:00:17 +0300

    Added task2

--- task.js
+++ task.js
@@ -13,3 +13,17 @@ console.log(thirdGuest);// Шелдон
 // 3. Выведите в консоль девятьсот девятого гостя
 const ninehundredninthGuest = guestList[908];
 console.log(ninehundredninthGuest); //underfined
+
+//task 2
+// Напишите функцию mergeArrays, которая принимает два массива и возвращает новый массив, объединяющий элементы обоих массивов с помощью оператора spread
+
+// Ваш код
+
+// Пример использования
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+// const mergedArray = mergeArrays(arr1, arr2);
+// console.log(mergedArray); // [1, 2, 3, 4, 5, 6]
+
+const mergedArray = [...arr1, ...arr2];
+console.log(mergedArray); // [1, 2, 3, 4, 5, 6]
