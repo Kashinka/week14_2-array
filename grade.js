0f7f73102ac552c3d2807a4ec1c30db66f630f10
Kashinka <azalena@yandex.ru>
2023-09-03 17:17:06 +0300

    Added average and comments

--- grade.js
+++ grade.js
@@ -1,1 +1,18 @@
 let grades = [];
+
+grades[0] = Math.floor(Math.random() * 101);
+grades[1] = Math.floor(Math.random() * 101);
+grades[2] = Math.floor(Math.random() * 101);
+grades[3] = Math.floor(Math.random() * 101);
+grades[4] = Math.floor(Math.random() * 101);
+grades[5] = Math.floor(Math.random() * 101);
+grades[6] = Math.floor(Math.random() * 101);
+grades[7] = Math.floor(Math.random() * 101);
+grades[8] = Math.floor(Math.random() * 101);
+grades[9] = Math.floor(Math.random() * 101);
+grades[10] = Math.floor(Math.random() * 101);
+grades[11] = Math.floor(Math.random() * 101); // заполняем массив баллами студентов от 1 до 100 с помощью рандомайзера
+console.log(grades);
+const sum = grades.reduce((a, b) => a + b, 0); // считаем сумму баллов студентов
+const average = (sum / grades.length) || 0; // считаем средний балл студентов
+console.log(average);
